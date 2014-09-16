bf5f37c3d5f23d6b0119ecf8ce1ff915f032d373
Andreas Olsson <olsson.andreas@gmail.com>
2014-09-16 17:44:26 +0200

    Omg

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
 require 'config/database.php';
 
-
+$loggedIn = false;
+if(isset($_SESSION['loggedIn'])) {
+	$loggedIn = $_SESSION['loggedIn'];
+}
 try {
 	$dbh = new PDO('mysql:host=localhost;dbname=websecurity', $DB_USERNAME, $DB_PASSWORD);
 	$error_message = "Seems like great success";
@@ -24,14 +27,20 @@ try {
 	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
 		<div class="container">
 			<a class="navbar-brand" href="/">Webshop</a>
-			<form class="navbar-form navbar-right" role="login" action="login.php">
+			<?php if(!$loggedIn) { ?>
+			<form class="navbar-form navbar-right" method="post" role="login" action="login.php">
 				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Username">
-					<input type="password" class="form-control" placeholder="Password">
+					<input type="text" name="username" class="form-control" placeholder="Username">
+					<input type="password" name="password" class="form-control" placeholder="Password">
 				</div>			
 				<button type="submit" class="btn btn-default">Login</button>
 				<a href="register.php" type="submit" class="btn btn-default">Register</a>
 			</form>
+			<?php } else { ?>
+			<form class="navbar-form navbar-right">
+				<a href="logout.php" type="submit" class="btn btn-danger">Log Out</a>
+			</form>
+			<?php } ?>
 		</div>
 	</nav>
 	<div class="jumbotron">
