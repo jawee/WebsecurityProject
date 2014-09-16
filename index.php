cc40b62ad60b280fa63463cae0fdef6245ad0b87
Andreas Olsson <olsson.andreas@gmail.com>
2014-09-16 16:12:14 +0200

    Testing

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php echo "Hello"; ?>
+<?php
+
+require 'config/database.php';
+
+
+try {
+	$dbh = new PDO('mysql:host=localhost;dbname=websecurity', $DB_USERNAME, $DB_PASSWORD);
+	echo "Seems like great success";
+} catch (PDOException $e) {
+	print "Error!: " . $e->getMessage();
+	die();
+}
+
+?>
+	
